81107387e2006b749a5f20879ffcc208aa25bc07
Eashan Gupta <eashangupta2@gmail.com>
2025-08-29 01:23:49 +0530

    Initial commit of Cypress automation project

---
+++ e2e/add_to_cart.cy.js
@@ -0,0 +1,19 @@
+import addtocart from '../page_object/InventoryPage'
+
+describe('Product Page Functionality', () => {
+    beforeEach(() => {
+        
+        cy.Login_in_site('eashan2@gmail.com', 'Password@1234');
+      
+    })
+
+    it('should allow a user to add an item to the cart', () => {
+      
+        const ad=new addtocart();
+        ad.visitproduct();
+        ad.clickonbluetop();
+        ad.continueshop();
+        ad.opencart();
+        ad.verifybluetop();
+    })
+}) 
