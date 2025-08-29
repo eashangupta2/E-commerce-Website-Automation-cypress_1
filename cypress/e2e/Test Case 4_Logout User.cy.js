7b8db15cf4084c766d70a03163ad7bc7c3490bca
Eashan Gupta <eashangupta2@gmail.com>
2025-08-29 23:28:24 +0530

    test case 4

---
+++ cypress/e2e/Test Case 4_Logout User.cy.js
@@ -0,0 +1,13 @@
+import login from "../page_object/LoginPage"
+describe('login test', () => {
+    it('login with valid password', () => {
+
+        const ln=new login();
+        ln.openpage();
+        ln.setusename("eashan2@gmail.com");
+        ln.setpassword("Password@1234");
+        ln.clicksubmit();
+        ln.verifylogin();
+        
+    })
+})
