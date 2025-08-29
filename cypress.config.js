18c8a2d67a345088095bbcd48e68b1b1c4c6fcb5
Eashan Gupta <eashangupta2@gmail.com>
2025-08-30 00:02:08 +0530

    fix: Remove cypress-image-snapshot due to dependency conflict

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Register image snapshot plugin for VRT
-      addMatchImageSnapshotPlugin(on, config);
+     
 
       // You can keep your existing node event listeners here if needed
     },
