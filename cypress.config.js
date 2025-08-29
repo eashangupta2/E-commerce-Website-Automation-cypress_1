bf2fe7216f73780154a3cac20b29cda581e7d8fc
Eashan Gupta <eashangupta2@gmail.com>
2025-08-30 00:08:23 +0530

    fix: Refactor config and clear CI cache

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,16 @@
 const { defineConfig } = require("cypress");
 
-
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-     
-
-      // You can keep your existing node event listeners here if needed
-    },
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'cypress/reports',
-      overwrite: false,
-      html: true,
-      json: true,
+      // implement node event listeners here
     },
   },
 });
