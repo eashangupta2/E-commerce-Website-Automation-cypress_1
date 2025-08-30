dbe62f0bf10481f47e214728755c76df5167473d
Eashan Gupta <eashangupta2@gmail.com>
2025-08-30 18:28:38 +0530

    fix: added day 27

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "utgouw", // Add your Project ID here
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
